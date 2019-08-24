bd0799bf98b97d544baa8cf66283524dbf69816a
paulocesarloayza@upsa.edu.bo <paulocesarloayza@upsa.edu.bo>
2019-08-23 21:00:43 -0400

    con entity framework  data context y conextion string

---
+++ contacto/contacto/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿
+namespace contacto.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
