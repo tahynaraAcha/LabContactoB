6e7af639da60003d4829c98d5e206cb237c25d40
paulocesarloayza@upsa.edu.bo <paulocesarloayza@upsa.edu.bo>
2019-09-20 21:16:06 -0400

    con apiservice

--- contacto/appContacto/appContacto/App.xaml.cs
+++ contacto/appContacto/appContacto/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using appContacto.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace appContacto
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new ListViewContact();
         }
 
         protected override void OnStart()
---
+++ contacto/appContacto/appContacto/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿namespace appContacto.Models
+{
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook
+    }
+
+    public class Contact
+    {
+        public int ContactID { get; set; }
+        public string Name { get; set; }
+        public TypeContact Type { get; set; }
+        public string ContactValue { get; set; }
+    }
+
+}
