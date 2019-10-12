8db668ff5b390d9fd41bf2cbbf2d6bc4294893b6
paulocesarloayza@upsa.edu.bo <paulocesarloayza@upsa.edu.bo>
2019-10-11 21:35:58 -0400

    viewmodel

--- contacto/appContacto/appContacto/ViewModels/ContactViewModel.cs
+++ contacto/appContacto/appContacto/ViewModels/ContactViewModel.cs
@@ -1,13 +1,70 @@
 ﻿namespace appContacto.ViewModels
 {
+    using appContacto.Models;
     using appContacto.Services;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Xamarin.Forms;
 
     public class ContactViewModel:BaseViewModel
     {
         #region Attributes
         ApiService apiService;
+        private ObservableCollection<Contact> contacts;
         #endregion
 
+        #region Properties
+        public ObservableCollection<Contact> Contacts
+        {
+            get { return this.contacts; }
+            set { SetValue(ref this.contacts, value); }
+        }
+        #endregion
+
+        #region Constructor
+        public ContactViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadContacts();
+        }
+        #endregion
+
+        #region Methods
+        private async void LoadContacts()
+        {
+            var connection = await this.apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Connection Error",
+                    connection.Message,
+                    "Accept"
+                    );
+                return;
+            }
+
+            var response = await this.apiService.GetList<Contact>(
+                        "http://localhost:50048/",
+                        "api/",
+                        "Contacts");
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "GET Contact Error",
+                    response.Message,
+                    "Accept"
+                    );
+                return;
+            }
+
+            MainViewModel mainViewModel = MainViewModel.GetInstance();
+            mainViewModel.ContactList = (List<Contact>)response.Result;
+
+            //this.Contacts = new ObservableCollection<Contact>();
+
+        } 
+        #endregion
 
     }
 }
--- contacto/appContacto/appContacto/ViewModels/MainViewModel.cs
+++ contacto/appContacto/appContacto/ViewModels/MainViewModel.cs
@@ -1,12 +1,23 @@
 ﻿namespace appContacto.ViewModels
 {
+    using appContacto.Models;
+    using System.Collections.Generic;
+
     public class MainViewModel
     {
+        #region Properties
+        public List<Contact> ContactList { get; set; }
+        #endregion
+
+        #region ViewModel
+        public ContactViewModel contactViewModel { get; set; }
+        #endregion
 
         #region Constructor
         public MainViewModel()
         {
             instance = this;
+            this.contactViewModel = new ContactViewModel();
         } 
         #endregion
 
